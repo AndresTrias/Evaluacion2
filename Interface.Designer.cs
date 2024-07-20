4858deb39d5cbabab74a613b7a41aa3d6f4824df
AndresTrias <otrias@estudiante.ceibal.edu.uy>
2024-07-20 18:15:09 -0300

    Evaluacion2
    vista

--- Interface.Designer.cs
+++ Interface.Designer.cs
@@ -30,6 +30,10 @@ namespace Evaluacion2
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.txtApellido = new System.Windows.Forms.TextBox();
@@ -39,115 +43,144 @@ namespace Evaluacion2
             this.btnModificacion = new System.Windows.Forms.Button();
             this.btnBaja = new System.Windows.Forms.Button();
             this.btnAlta = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.txtApellido);
-            this.panel1.Controls.Add(this.txtNombre);
-            this.panel1.Controls.Add(this.btnBuscar);
-            this.panel1.Controls.Add(this.btnListar);
-            this.panel1.Controls.Add(this.btnModificacion);
-            this.panel1.Controls.Add(this.btnBaja);
-            this.panel1.Controls.Add(this.btnAlta);
-            this.panel1.Location = new System.Drawing.Point(193, 65);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(436, 382);
+            this.panel1.Size = new System.Drawing.Size(95, 571);
             this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(95, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(662, 98);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(95, 479);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(662, 92);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(674, 98);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(83, 381);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.panel5.Controls.Add(this.textBox2);
+            this.panel5.Controls.Add(this.textBox1);
+            this.panel5.Controls.Add(this.txtApellido);
+            this.panel5.Controls.Add(this.txtNombre);
+            this.panel5.Controls.Add(this.btnBuscar);
+            this.panel5.Controls.Add(this.btnListar);
+            this.panel5.Controls.Add(this.btnModificacion);
+            this.panel5.Controls.Add(this.btnBaja);
+            this.panel5.Controls.Add(this.btnAlta);
+            this.panel5.Location = new System.Drawing.Point(95, 98);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(579, 381);
+            this.panel5.TabIndex = 4;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(118, 142);
+            this.textBox2.Location = new System.Drawing.Point(197, 146);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(180, 26);
-            this.textBox2.TabIndex = 18;
+            this.textBox2.TabIndex = 27;
             this.textBox2.Text = "Confira Contrasenia";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(118, 103);
+            this.textBox1.Location = new System.Drawing.Point(197, 107);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(180, 26);
-            this.textBox1.TabIndex = 17;
+            this.textBox1.TabIndex = 26;
             this.textBox1.Text = "Contrasenia";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtApellido
             // 
             this.txtApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtApellido.Location = new System.Drawing.Point(118, 69);
+            this.txtApellido.Location = new System.Drawing.Point(197, 73);
             this.txtApellido.Name = "txtApellido";
             this.txtApellido.Size = new System.Drawing.Size(180, 26);
-            this.txtApellido.TabIndex = 16;
+            this.txtApellido.TabIndex = 25;
             this.txtApellido.Text = "Apellido";
             this.txtApellido.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtNombre
             // 
             this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNombre.Location = new System.Drawing.Point(118, 37);
+            this.txtNombre.Location = new System.Drawing.Point(197, 41);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(180, 26);
-            this.txtNombre.TabIndex = 15;
+            this.txtNombre.TabIndex = 24;
             this.txtNombre.Text = "Nombre";
             this.txtNombre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btnBuscar
             // 
             this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBuscar.Location = new System.Drawing.Point(54, 305);
+            this.btnBuscar.Location = new System.Drawing.Point(133, 309);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(146, 31);
-            this.btnBuscar.TabIndex = 14;
+            this.btnBuscar.TabIndex = 23;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
             // 
             // btnListar
             // 
             this.btnListar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnListar.Location = new System.Drawing.Point(221, 305);
+            this.btnListar.Location = new System.Drawing.Point(300, 309);
             this.btnListar.Name = "btnListar";
             this.btnListar.Size = new System.Drawing.Size(146, 31);
-            this.btnListar.TabIndex = 13;
+            this.btnListar.TabIndex = 22;
             this.btnListar.Text = "Listar";
             this.btnListar.UseVisualStyleBackColor = true;
             // 
             // btnModificacion
             // 
             this.btnModificacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnModificacion.Location = new System.Drawing.Point(221, 251);
+            this.btnModificacion.Location = new System.Drawing.Point(300, 255);
             this.btnModificacion.Name = "btnModificacion";
             this.btnModificacion.Size = new System.Drawing.Size(146, 31);
-            this.btnModificacion.TabIndex = 12;
+            this.btnModificacion.TabIndex = 21;
             this.btnModificacion.Text = "Modificacion";
             this.btnModificacion.UseVisualStyleBackColor = true;
             // 
             // btnBaja
             // 
             this.btnBaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBaja.Location = new System.Drawing.Point(54, 251);
+            this.btnBaja.Location = new System.Drawing.Point(133, 255);
             this.btnBaja.Name = "btnBaja";
             this.btnBaja.Size = new System.Drawing.Size(146, 31);
-            this.btnBaja.TabIndex = 11;
+            this.btnBaja.TabIndex = 20;
             this.btnBaja.Text = "Eliminar";
             this.btnBaja.UseVisualStyleBackColor = true;
             // 
             // btnAlta
             // 
             this.btnAlta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAlta.Location = new System.Drawing.Point(135, 183);
+            this.btnAlta.Location = new System.Drawing.Point(214, 187);
             this.btnAlta.Name = "btnAlta";
             this.btnAlta.Size = new System.Drawing.Size(146, 31);
-            this.btnAlta.TabIndex = 10;
+            this.btnAlta.TabIndex = 19;
             this.btnAlta.Text = "Registrar";
             this.btnAlta.UseVisualStyleBackColor = true;
             // 
@@ -155,13 +188,17 @@ namespace Evaluacion2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1284, 729);
+            this.ClientSize = new System.Drawing.Size(757, 571);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Interface";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Interface";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -169,6 +206,10 @@ namespace Evaluacion2
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox txtApellido;
